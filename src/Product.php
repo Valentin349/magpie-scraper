1ca149a184d766557ea1cc56f2a12c019e4f9640
valentin349 <valentino7007@gmail.com>
2023-12-06 12:04:16 +0000

    final code

--- src/Product.php
+++ src/Product.php
@@ -4,5 +4,116 @@ namespace App;
 
 class Product
 {
+    private string $baseUrl;
+    private string $title;
+    private string $capacityText;
+    private string $price;
+    private string $imgUrl;
+    private string $color;
+    private string $availabilityText;
+    private string $shippingText = "";
 
+    function __construct($productElement, $colorElement, $url)
+    {
+        $this->baseUrl = $url;
+
+        $this->title = $productElement->filter('.product-name')->text();
+        $this->capacityText = $productElement->filter('.product-capacity')->text();
+        $this->price = $productElement->filter('.my-8.block.text-center.text-lg')->text();
+        $this->imgUrl = $productElement->filter('img')->attr('src');
+        $this->color = $colorElement->attr('data-colour');
+
+        $availabilityElement = $productElement->filter('.my-4.text-sm.block.text-center');
+        $this->availabilityText = $availabilityElement->text();
+
+        // check if node list empty
+        if (count($availabilityElement) > 1){
+            $this->shippingText = $availabilityElement->nextAll('.my-4.text-sm.block.text-center')->text();
+        }
+
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title . " " . $this->capacityText;
+    }
+
+    public function getCapacity(): float
+    {
+
+        $multiplier = 1;
+
+        if (stripos($this->capacityText, 'GB') !== false) {
+            $multiplier = 1024;
+        }
+
+        preg_match('/[\d.]+/', $this->capacityText, $matches);
+        $capacity = (float)$matches[0] * $multiplier;
+
+        return $capacity;
+    }
+
+    public function getImgUrl(): string
+    {
+        return $this->baseUrl . '/' . ltrim($this->imgUrl, './');
+    }
+
+    public function getPrice(): float
+    {
+        return floatval(preg_replace('/[^0-9.]/', '', $this->price));
+    }
+
+    public function getColor(): string
+    {
+        return $this->color;
+    }
+
+    public function getAvailabilityText(): string
+    {
+        return $this->availabilityText;
+    }
+
+    public function getIsAvailable(): bool
+    {
+        return stripos($this->availabilityText, 'In Stock') !== false;
+    }
+
+    public function getShippingText(): string
+    {
+        return $this->shippingText;
+    }
+
+    public function getShippingDate(): string
+    {
+        $matches = [];
+        $patterns = [
+            '/(\d{4}-\d{2}-\d{2})/',   
+            '/(\d{2} \w+ \d{4})/',            
+            '/(\d{1,2} \w+ \d{4})/',          
+            '/(\d{4}-\d{2}-\d{2})/',          
+            '/\b(\d{1,2} \w+ \d{4})\b/',
+            '/(\w+ \d{1,2}(?:st|nd|rd|th) \w+ \d{4})/',
+            '/(?:\b\w+\b\s+)?(?:today|tomorrow)/i'    
+        ];
+
+        foreach ($patterns as $pattern) {
+            if (preg_match($pattern, $this->shippingText, $matches)) {
+                // check if preg_match adds a value to $matches
+                $date = isset($matches[1]) ? $matches[1] : $matches[0];
+
+                if (strtolower(trim($date)) === 'today') {
+                    $date = date('Y-m-d');
+                } elseif (strtolower(trim($date)) === 'tomorrow') {
+                    $date = date('Y-m-d', strtotime('+1 day'));
+                } else {
+                    $date = date('Y-m-d', strtotime($date));
+                }
+
+                return $date;
+            }
+        }
+
+        return "";
+    }
+    
 }
--- src/Scrape.php
+++ src/Scrape.php
@@ -2,17 +2,67 @@
 
 namespace App;
 
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
 
 class Scrape
 {
     private array $products = [];
 
     public function run(): void
-    {
-        $document = ScrapeHelper::fetchDocument('https://www.magpiehq.com/developer-challenge/smartphones');
+    {   
+        $baseUrl = 'https://www.magpiehq.com/developer-challenge/smartphones';
+        $currentPage = 1;
+        do {
+            $url = $baseUrl . "/?page=" . $currentPage;
+            $document = ScrapeHelper::fetchDocument($url);
 
-        file_put_contents('output.json', json_encode($this->products));
+            $productElements = $document->filter('.product');
+            if ($productElements->count() > 0) {
+                $productElements->each(function ($productElement) use ($baseUrl) {
+
+                    $colorElements = $productElement->filter('.border.border-black.rounded-full.block');
+                    $colorElements->each(function ($colorElement) use ($productElement, $baseUrl) {
+                        
+                        $product = new Product($productElement, $colorElement, $baseUrl);
+                        
+                        $uniqueIdentifier = $product->getTitle() . '_' . $product->getColor();
+        
+                        if (!$this->isDuplicate($uniqueIdentifier)) {
+                            $this->products[] = [
+                                'title' => $product->getTitle(),
+                                'price' => $product->getPrice(),
+                                'imgUrl' => $product->getImgUrl(),
+                                'capacityMB' => $product->getCapacity(),
+                                'color' => $product->getColor(),
+                                'availabilityText' => $product->getAvailabilityText(),
+                                'isAvailable' => $product->getIsAvailable(),
+                                'shippingText' => $product->getShippingText(),
+                                'shippingDate' => $product->getShippingDate()
+                            ];
+                        }
+                    });
+                });
+
+                $currentPage++;
+                
+            } else {
+                break;
+            }
+        } while (true);
+
+
+        file_put_contents('output.json', json_encode($this->products, JSON_UNESCAPED_SLASHES));
+    }
+
+    private function isDuplicate(string $uniqueIdentifier): bool
+    {
+        foreach ($this->products as $existingProduct) {
+            $existingIdentifier = $existingProduct['title'] . '_' . $existingProduct['color'];
+            if ($uniqueIdentifier === $existingIdentifier) {
+                return true;
+            }
+        }
+        return false;
     }
 }
 
